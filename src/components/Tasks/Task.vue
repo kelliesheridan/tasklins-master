ba39ef6d195e2beb1b29f0444dcf6a5349ac8be2
Kellie Sheridan <kellie@snarkybooks.com>
2020-08-24 12:19:53 -0400

    Due Time

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -30,6 +30,11 @@
             caption>
             {{ task.dueDate | niceDate }}
           </q-item-label>
+          <!-- <q-item-label
+            class="row justify-end"
+            caption>
+            <small>{{ taskDueTime }}</small>
+          </q-item-label> -->
         </div>
       </div>
     </q-item-section>
