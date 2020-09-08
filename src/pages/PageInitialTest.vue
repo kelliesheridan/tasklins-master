0c13c3f4bbcaebdba44c73b2a9c2da1f7b996377
Kellie Sheridan <kellie@snarkybooks.com>
2020-09-08 18:18:18 -0400

    egg stuff

--- src/pages/PageInitialTest.vue
+++ src/pages/PageInitialTest.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <div class="egg1">
+    <div :class="eggColor" class="egg1">
       <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         viewBox="0 0 300 300" style="enable-background:new 0 0 300 300;" xml:space="preserve">
 
@@ -8,16 +8,15 @@
         <path class="st0" d="M236.39,82.84c19.41,43.02,21.09,77.63,12.42,115.93c-8.68,38.39-33.04,62.5-56,75.65
           c-47.27,27.08-89.36,10.14-118.47-24.18C28.4,196.09,43.65,100.61,84.53,49.26C138.86-19,206.09,15.69,236.39,82.84L236.39,82.84z"
           />
-        <path class="st1" d="M185.54,84.21c4.65,26.89,0.58,46.51-9.81,66.71c-10.41,20.24-27.54,30.22-42.3,34.26
+        <path class="st1 eggHighlight" d="M185.54,84.21c4.65,26.89,0.58,46.51-9.81,66.71c-10.41,20.24-27.54,30.22-42.3,34.26
           c-30.39,8.33-51.5-7.25-62.84-30.67c-17.89-36.97,4.45-88.22,34.76-111.07C145.66,13.08,178.29,42.22,185.54,84.21L185.54,84.21z"
           />
-        <path class="st2" d="M236.39,82.84c19.41,43.02,21.09,77.63,12.42,115.93c-8.68,38.39-33.04,62.5-56,75.65
+        <path class="st2 eggShadow" d="M236.39,82.84c19.41,43.02,21.09,77.63,12.42,115.93c-8.68,38.39-33.04,62.5-56,75.65
           c-47.27,27.08-89.36,10.14-118.47-24.18c-1.97-2.33-3.83-4.74-5.58-7.21c51.54,34.83,89.11,26.34,121.36-0.87
           c64.22-54.17,55.21-141.91,20.6-199.1C220.78,54.12,229.53,67.64,236.39,82.84L236.39,82.84z"/>
       </g>
       </svg>
     </div>
-
 
        
   </q-page>
@@ -29,10 +28,13 @@ import { mapActions, mapState, mapGetters } from "vuex";
 export default {
   data() {
     return {
-      
+      hex: 'blue',
+      eggColor: 'eggStarterCyan'
     };
   },
   computed: {
+    ...mapGetters("profile", ["profile"]),
+    
      },
   components: {
     
@@ -43,10 +45,20 @@ export default {
 <style type="text/css">
 
 .egg1 {
-  max-width: 200px;
+  max-width: 250px;
 }
-	.st0{fill-rule:evenodd;clip-rule:evenodd;fill:#8388AA;}
-	.st1{fill-rule:evenodd;clip-rule:evenodd;fill:#ACAFC6;}
-  .st2{fill-rule:evenodd;clip-rule:evenodd;fill:#606693;}
+	.st0{fill-rule:evenodd;clip-rule:evenodd;}
+	.st1{fill-rule:evenodd;clip-rule:evenodd;}
+  .st2{fill-rule:evenodd;clip-rule:evenodd;}
+
+  .eggShadow {
+    fill: black;
+    opacity: .3;
+}
+
+  .eggHighlight {
+    fill: white;
+    opacity: .3;
+}
   
 </style>
