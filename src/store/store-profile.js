942a8d50a79106a59d89178af24393425809cab0
Kellie Sheridan <kellie@snarkybooks.com>
2020-09-13 15:41:10 -0400

    undo notify

--- src/store/store-profile.js
+++ src/store/store-profile.js
@@ -240,11 +240,6 @@ function getLevel() {
     level = "1";
   }
   if (level > state.profile.user.level) {
-    this.$q.notify({
-      message: "Congratulations, You've leveled up! Welcome to level " + level,
-      color: "primary",
-      icon: "favorite"
-    });
     alert("Congratulations, You've leveled up! Welcome to level " + level)
   }
   return level;
