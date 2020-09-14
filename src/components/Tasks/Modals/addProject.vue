43d289327e477493b2cfd419aa4a703ee4f589eb
mgloverBSI <mglover@burstingsilver.com>
2020-09-13 23:30:29 -0400

    projects

--- src/components/Tasks/Modals/addProject.vue
+++ src/components/Tasks/Modals/addProject.vue
@@ -1,84 +1,86 @@
 <template>
-   <q-card style="width: 550px">
-        <modal-header>Add New Project</modal-header>
+  <q-card style="width: 550px">
+    <modal-header>Add New Project</modal-header>
 
-        <q-form @submit.prevent="submitForm">
+    <q-form @submit.prevent="submitForm">
+      <q-card-section class="q-pt-none">
+        <q-input
+          outlined
+          v-model="project.projectName"
+          :rules="[val => !!val || 'Field is required']"
+          autofocus
+          ref="name"
+          label="Project name *"
+          class="col"
+        >
+        </q-input>
 
-            <q-card-section class="q-pt-none">
-
-            <q-input
-                outlined
-                v-model="project.projectName"
-                :rules="[val => !!val || 'Field is required']"
-                autofocus
-                ref="name"                
-                label="Project name *"
-                class="col">
-            </q-input>
-
-            <!-- <q-select
+        <!-- <q-select
                 outlined
                 v-model="project.projectName"
                 :options="this.$store.state.tasks.projects">
             </q-select> -->
 
-            <div>
-                <div>
-                    <p class="text-center"> Soon, you'll be able to assign each project to a Tasklin. For now, your starter Tasklin will gain experience on all projects.</p>
-                </div>
-                <div>
-                    <q-select
-                        disable
-                        readonly
-                        filled
-                        label="Tasks"
-                        v-model="project.projectTasklin"
-                        :options="this.$store.state.tasks.projects"                  
-                    />
-                </div>                
-            </div>             
+        <div>
+          <div>
+            <p class="text-center">
+              Soon, you'll be able to assign each project to a Tasklin. For now,
+              your starter Tasklin will gain experience on all projects.
+            </p>
+          </div>
+          <div>
+            <q-select
+              disable
+              readonly
+              filled
+              label="Tasks"
+              v-model="project.projectTasklin"
+              :options="this.$store.state.tasks.projects"
+            />
+          </div>
+        </div>
+      </q-card-section>
 
-        </q-card-section>
-
-        <modal-task-save></modal-task-save>	    
-        </q-form>
-    </q-card>
+      <modal-task-save></modal-task-save>
+    </q-form>
+  </q-card>
 </template>
 
 <script>
-import { mapState, mapActions, mapGetters } from 'vuex'
+import { mapState, mapActions, mapGetters } from "vuex";
 
 export default {
-    data() {
-        return {
-            project: {
-                projectName: '',
-                color: '',
-            },
-                  options: ['Tasks']
-        }
-    },
-    computed: {
-        ...mapState({
-            message: state => state.state
-        }),
+  data() {
+    return {
+      project: {
+        projectName: "",
+        color: ""
+      },
+      options: ["Tasks"]
+    };
+  },
+  computed: {
+    ...mapState({
+      message: state => state.state
+    })
+  },
+  components: {
+    "modal-header": require("components/Tasks/Modals/Shared/ModalHeader.vue")
+      .default,
+    "modal-task-save": require("components/Tasks/Modals/Shared/ModalTaskSave.vue")
+      .default
+  },
+  methods: {
+    ...mapActions("tasks", ["addProject"]),
+    ...mapGetters("profile", ["profile"]),
 
-        },
-    components: {
-    'modal-header': require('components/Tasks/Modals/Shared/ModalHeader.vue').default,    
-    'modal-task-save': require('components/Tasks/Modals/Shared/ModalTaskSave.vue').default,
+    submitProject() {
+      this.addProject(this.project);
+      this.$emit("close");
     },
-    methods: {
-        ...mapActions('tasks', ['addProject']),
-        ...mapGetters('profile', ['profile']),
-
-        submitProject() {
-            this.addProject(this.project)
-            this.$emit('close')
-        },
-        submitForm() {
-            this.submitProject()
-        },
-    },
-}
+    submitForm() {
+      this.submitProject();
+    }
+  }
+};
 </script>
