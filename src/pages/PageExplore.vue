b24152e74952d5240747abd264a67c9617627ac1
Kellie Sheridan <kellie@snarkybooks.com>
2020-10-01 17:00:13 -0400

    Page Explore

--- src/pages/PageExplore.vue
+++ src/pages/PageExplore.vue
@@ -1,9 +1,12 @@
 <template>
   <q-page padding>
       <div class="world-map">
-        <q-img usemap="#worldmap"
+        <q-img
         class="world-map rounded-borders" src="statics/backgrounds/WorldMap.jpg">
         </q-img>
+        <list-header class="textureBar explore-bar">
+          <div>Let's be honest... this page doesn't do anything yet. But one of the focuses for October is being able to go into the world of Tasklins and start exploring!</div>
+        </list-header>
       </div>
   </q-page>
 </template>
@@ -11,7 +14,7 @@
 <script>
 export default {
     components: {
-    // "list-header": require("components/Shared/ListHeader.vue").default,
+    "list-header": require("components/Shared/ListHeader.vue").default,
   },
 }
 </script>
