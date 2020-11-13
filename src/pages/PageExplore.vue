85dbdd0f673cdcfc3aa70fdcd88fb7b1b936bbf9
mgloverBSI <mglover@burstingsilver.com>
2020-11-12 20:25:14 -0500

    fix kellie's broken stuff

--- src/pages/PageExplore.vue
+++ src/pages/PageExplore.vue
@@ -1,25 +1,22 @@
 <template>
   <q-page padding>
-    <div class="row section-world justify-center">
-
-      <div class="section-world-menu col-xs-12 col-sm-12 col-md-2 col-lg-2 q-pa-xs">
-        <div class="q-pa-md q-gutter-sm">
-          <q-btn @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Town" color="secondary" />
+    <div class="row">
+    <div class="col-lg-2">
+        <q-btn @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Town" color="secondary" />
           <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Forest" color="secondary" />
           <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Mountain" color="secondary" />
           <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="City" color="secondary" />
           <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Island" color="secondary" />
           <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Mountains" color="secondary" />
           <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Floating" color="secondary" />
-          
-        </div>
-      </div>
+       
+    </div>
+    <div class="col-lg-10">
+            <div class="world-map" style="width: 80%; height: 80%; position: absolute" :class="!darkMode ? 'world-day' : 'world-night'">   </div> 
+            <div v-show="showTown" class="world-map map-town" style="width: 80%; height: 80%;  margin-top: 0%;  position: absolute;"></div>   
+    </div>
+    </div>
 
-      
-      
-      <div class="world-map col-xs-12 col-sm-12 col-md-10 col-lg-10 q-pa-xs" :class="!darkMode ? 'world-day' : 'world-night'">   </div> 
-      <div class="world-map map-town col-xs-12 col-sm-12 col-md-10 col-lg-10 q-pa-xs" style="margin-top: -100%; position: absolute"></div>   
-    </div> 
   </q-page>
 </template>
 
