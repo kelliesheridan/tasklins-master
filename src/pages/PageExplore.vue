6c758fe8e6f98b610c9ec075d4e3db69216d2be5
Kellie Sheridan <kellie@snarkybooks.com>
2020-11-12 22:51:26 -0500

    map stuff

--- src/pages/PageExplore.vue
+++ src/pages/PageExplore.vue
@@ -1,19 +1,20 @@
 <template>
   <q-page padding>
-    <div class="row">
-    <div class="col-lg-2">
-        <q-btn @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Town" color="secondary" />
-          <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Forest" color="secondary" />
-          <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Mountain" color="secondary" />
-          <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="City" color="secondary" />
-          <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Island" color="secondary" />
-          <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Mountains" color="secondary" />
-          <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width block q-pa-xs" push to="/start/pick-quasar-flavour" label="Floating" color="secondary" />
-       
+    <div class="q-gutter-sm" style="width: 75%;">
+    <div class="center">
+      <q-btn @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width q-ma-sm" push to="/town" label="Town" color="secondary" />
+      <q-btn disable @mouseover="showForest = true" @mouseleave="showForest = false" class="btn-fixed-width q-ma-sm" push to="/start/pick-quasar-flavour" label="Forest" color="secondary" />
+      <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width q-ma-sm" push to="/start/pick-quasar-flavour" label="Mountain" color="secondary" />
+      <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width q-ma-sm" push to="/start/pick-quasar-flavour" label="City" color="secondary" />
+      <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width q-ma-sm" push to="/start/pick-quasar-flavour" label="Island" color="secondary" />
+      <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width q-ma-sm" push to="/start/pick-quasar-flavour" label="Mountains" color="secondary" />
+      <q-btn disable @mouseover="showTown = true" @mouseleave="showTown = false" class="btn-fixed-width q-ma-sm" push to="/start/pick-quasar-flavour" label="Floating" color="secondary" />
     </div>
-    <div class="col-lg-10">
-            <div class="world-map" style="width: 80%; height: 80%; position: absolute" :class="!darkMode ? 'world-day' : 'world-night'">   </div> 
-            <div v-show="showTown" class="world-map map-town" style="width: 80%; height: 80%;  margin-top: 0%;  position: absolute;"></div>   
+
+    <div>
+      <div class="world-map" style="width: 90%; height: 90%; position: absolute;" :class="!darkMode ? 'world-day' : 'world-night'">   </div> 
+      <div v-show="showTown" class="world-map map-town" style="width: 90%; height: 90%;  margin-top: 0%;  position: absolute;"></div>
+      <div v-show="showForest" class="world-map map-forest" style="width: 90%; height: 90%;  margin-top: 0%;  position: absolute;"></div>      
     </div>
     </div>
 
@@ -27,7 +28,8 @@ export default {
 
     data() {
     return {
-      showTown: false
+      showTown: false,
+      showForest: false
     };
   },
   computed: {
---
+++ src/pages/map/town.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="fixed-center text-center">
+    <p>
+      <img
+        src="statics/sad404.png"
+        style="width:30vw;max-width:150px;">
+    </p>
+    <p class="text-faded">
+      Look, you found a town!
+    </p>
+    <q-btn
+      color="secondary"
+      style="width:200px;"
+      to="/"
+      label="Go back"
+    />
+  </div>
+</template>
+
+<script >
+export default {
+  name: 'Error404'
+}
+</script>
