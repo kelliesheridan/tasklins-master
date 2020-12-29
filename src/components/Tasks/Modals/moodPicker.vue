c463a0a7b4d5746f765d91a25baab0d7177792ab
mgloverBSI <mglover@burstingsilver.com>
2020-12-29 18:11:28 -0500

    mood picker

--- src/components/Tasks/Modals/moodPicker.vue
+++ src/components/Tasks/Modals/moodPicker.vue
@@ -1,37 +1,38 @@
 <template>
   <q-card style="width: 420px">
-      <list-header class="textureBar">
-        {{ mood }} 
-      </list-header>
+    <list-header class="textureBar">
+      {{ mood }}
+    </list-header>
 
     <div class="q-pa-md">
-      <q-form
-        @submit="setUserMood()"
-        class="q-gutter-md"
-      >
-      <q-img
-        src="/statics/moods/happy.png"
-        spinner-color="white"
-        style="height: 100px; max-width: 100px"
-        @click="mood = 'happy'"
-      />
-      <q-img
-        src="/statics/moods/fine.png"
-        spinner-color="white"
-        style="height: 100px; max-width: 100px"
-        @click="mood = 'fine'"
-      />
-      <q-img
-        src="/statics/moods/glum.png"
-        spinner-color="white"
-        style="height: 100px; max-width: 100px"
-        @click="mood = 'glum'"
-      />
-      
+      <q-form @submit="setUserMood()" class="q-gutter-md">
+        <q-img
+          src="/statics/moods/happy.png"
+          spinner-color="white"
+          style="height: 100px; max-width: 100px"
+          @click="mood = 'happy'"
+        />
+        <q-img
+          src="/statics/moods/fine.png"
+          spinner-color="white"
+          style="height: 100px; max-width: 100px"
+          @click="mood = 'fine'"
+        />
+        <q-img
+          src="/statics/moods/glum.png"
+          spinner-color="white"
+          style="height: 100px; max-width: 100px"
+          @click="mood = 'glum'"
+        />
+
         <q-input filled v-model="date" mask="date">
           <template v-slot:append>
             <q-icon name="event" class="cursor-pointer">
-              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+              <q-popup-proxy
+                ref="qDateProxy"
+                transition-show="scale"
+                transition-hide="scale"
+              >
                 <q-date v-model="date">
                   <div class="row items-center justify-end">
                     <q-btn v-close-popup label="Close" color="primary" flat />
@@ -43,12 +44,11 @@
         </q-input>
 
         <div>
-          <q-btn label="Submit" type="submit" color="primary"/>
+          <q-btn label="Submit" type="submit" color="primary" />
         </div>
       </q-form>
-
     </div>
-    </q-card>
+  </q-card>
 </template>
 
 <script>
@@ -59,23 +59,23 @@ export default {
   data() {
     return {
       mood: " ",
-      date: moment().format("YYYY-MM-DD"),
-    }
+      date: moment().format("YYYY-MM-DD")
+    };
   },
-  computed: {
-
-  },
+  computed: {},
   components: {
-        "list-header": require("components/Shared/ListHeader.vue").default,
+    "list-header": require("components/Shared/ListHeader.vue").default
   },
   methods: {
     ...mapActions("profile", ["setMood"]),
     setUserMood() {
-      let mood = {
-        mood: this.mood,
-        moodDate: moment(this.date).format("YYYY-MM-DD"),
+      if (this.mood != "") {
+        let mood = {
+          mood: this.mood,
+          moodDate: moment(this.date).format("YYYY-MM-DD")
+        };
+        this.setMood(mood);
       }
-      this.setMood(mood);
     }
   }
 };
