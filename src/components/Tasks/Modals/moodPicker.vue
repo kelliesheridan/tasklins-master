62252002808dc6f2b9b7ae9bb827cf862383f17f
kelliesheridan <45521626+kelliesheridan@users.noreply.github.com>
2020-12-29 17:21:36 -0500

    mood picker

--- src/components/Tasks/Modals/moodPicker.vue
+++ src/components/Tasks/Modals/moodPicker.vue
@@ -1,25 +1,54 @@
 <template>
-  <q-card style="width: 550px">
-    <div class="center">{{ mood }}</div>
-    <q-img
-      src="/statics/moods/happy.png"
-      spinner-color="white"
-      style="height: 100px; max-width: 100px"
-      @click="mood = 'happy'; setUserMood(mood)"
-    />
-    <q-img
-      src="/statics/moods/fine.png"
-      spinner-color="white"
-      style="height: 100px; max-width: 100px"
-      @click="mood = 'fine'; setUserMood(mood)"
-    />
-    <q-img
-      src="/statics/moods/glum.png"
-      spinner-color="white"
-      style="height: 100px; max-width: 100px"
-      @click="mood = 'glum'; setUserMood(mood)"
-    />
-  </q-card>
+  <q-card style="width: 420px">
+      <list-header class="textureBar">
+        {{ mood }} 
+      </list-header>
+
+    <div class="q-pa-md">
+      <q-form
+        @submit="onSubmit"
+        class="q-gutter-md"
+      >
+      <q-img
+        src="/statics/moods/happy.png"
+        spinner-color="white"
+        style="height: 100px; max-width: 100px"
+        @click="mood = 'happy'; setUserMood(mood)"
+      />
+      <q-img
+        src="/statics/moods/fine.png"
+        spinner-color="white"
+        style="height: 100px; max-width: 100px"
+        @click="mood = 'fine'; setUserMood(mood)"
+      />
+      <q-img
+        src="/statics/moods/glum.png"
+        spinner-color="white"
+        style="height: 100px; max-width: 100px"
+        @click="mood = 'glum'; setUserMood(mood)"
+      />
+      
+        <q-input filled v-model="date" mask="date" :rules="['date']">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                <q-date v-model="date">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+        </div>
+      </q-form>
+
+    </div>
+    </q-card>
 </template>
 
 <script>
@@ -29,12 +58,14 @@ export default {
   data() {
     return {
       mood: " ",
+      date: null,
     }
   },
   computed: {
 
   },
   components: {
+        "list-header": require("components/Shared/ListHeader.vue").default,
   },
   methods: {
     ...mapActions("profile", ["setMood"]),
