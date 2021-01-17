9b98422a5af5dba2cb798c6793071224a76080cc
mgloverBSI <mglover@burstingsilver.com>
2021-01-17 00:18:38 -0500

    working

--- src/components/Tasks/Modals/Shared/ModalTaskRepeating.vue
+++ src/components/Tasks/Modals/Shared/ModalTaskRepeating.vue
@@ -5,9 +5,9 @@
         class="task-btn q-pa-xs"
         @click="
           daily = !daily;
+          everyMonth = false;
           byDayNumber = false;
           everyNumDays = false;
-          everyMonth = false;
           nrepeating.numDay = '';
           nrepeating.numDaySet = false;
         "
@@ -25,6 +25,7 @@
         class="task-btn q-pa-xs"
         @click="
           daily = false;
+          everyMonth = false;
           byDayNumber = false;
           everyNumDays = !everyNumDays;
           nrepeating.monday = false;
@@ -34,7 +35,6 @@
           nrepeating.friday = false;
           nrepeating.saturday = false;
           nrepeating.sunday = false;
-          everyMonth = false;
         "
         flat
         dense
@@ -55,7 +55,7 @@
           everyNumDays = false;
           nrepeating.numDay = '';
           nrepeating.numDaySet = false;
-          //nrepeating.monthly = everyMonth;
+          nrepeating.monthly = everyMonth;
         "
         @change="$emit('update:nrepeating.monthly', $event)"
         flat
@@ -149,364 +149,6 @@
           />
         </span>
 
-        <div v-if="monthly">
-          <div>
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="1"
-              :value="nrepeating.monday"
-              @input="$emit('update:nrepeating.monday', $event)"
-              :class="!nrepeating.monday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.monday = !nrepeating.monday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="2"
-              :value="nrepeating.tuesday"
-              @input="$emit('update:nrepeating.tuesday', $event)"
-              :class="!nrepeating.tuesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.tuesday = !nrepeating.tuesday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="3"
-              :value="nrepeating.wednesday"
-              @input="$emit('update:nrepeating.wednesday', $event)"
-              :class="!nrepeating.wednesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.wednesday = !nrepeating.wednesday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="4"
-              :value="nrepeating.thursday"
-              @input="$emit('update:nrepeating.thursday', $event)"
-              :class="!nrepeating.thursday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.thursday = !nrepeating.thursday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="5"
-              :value="nrepeating.friday"
-              @input="$emit('update:nrepeating.friday', $event)"
-              :class="!nrepeating.friday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.friday = !nrepeating.friday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="6"
-              :value="nrepeating.saturday"
-              @input="$emit('update:nrepeating.saturday', $event)"
-              :class="!nrepeating.saturday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.saturday = !nrepeating.saturday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="7"
-              :value="nrepeating.sunday"
-              @input="$emit('update:nrepeating.sunday', $event)"
-              :class="!nrepeating.sunday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.sunday = !nrepeating.sunday"
-            />
-          </div>
-
-          <div>
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="8"
-              :value="nrepeating.monday"
-              @input="$emit('update:nrepeating.monday', $event)"
-              :class="!nrepeating.monday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.monday = !nrepeating.monday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="9"
-              :value="nrepeating.tuesday"
-              @input="$emit('update:nrepeating.tuesday', $event)"
-              :class="!nrepeating.tuesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.tuesday = !nrepeating.tuesday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="10"
-              :value="nrepeating.wednesday"
-              @input="$emit('update:nrepeating.wednesday', $event)"
-              :class="!nrepeating.wednesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.wednesday = !nrepeating.wednesday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="11"
-              :value="nrepeating.thursday"
-              @input="$emit('update:nrepeating.thursday', $event)"
-              :class="!nrepeating.thursday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.thursday = !nrepeating.thursday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="12"
-              :value="nrepeating.friday"
-              @input="$emit('update:nrepeating.friday', $event)"
-              :class="!nrepeating.friday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.friday = !nrepeating.friday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="13"
-              :value="nrepeating.saturday"
-              @input="$emit('update:nrepeating.saturday', $event)"
-              :class="!nrepeating.saturday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.saturday = !nrepeating.saturday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="14"
-              :value="nrepeating.sunday"
-              @input="$emit('update:nrepeating.sunday', $event)"
-              :class="!nrepeating.sunday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.sunday = !nrepeating.sunday"
-            />
-          </div>
-
-          <div>
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="15"
-              :value="nrepeating.monday"
-              @input="$emit('update:nrepeating.monday', $event)"
-              :class="!nrepeating.monday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.monday = !nrepeating.monday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="16"
-              :value="nrepeating.tuesday"
-              @input="$emit('update:nrepeating.tuesday', $event)"
-              :class="!nrepeating.tuesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.tuesday = !nrepeating.tuesday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="17"
-              :value="nrepeating.wednesday"
-              @input="$emit('update:nrepeating.wednesday', $event)"
-              :class="!nrepeating.wednesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.wednesday = !nrepeating.wednesday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="18"
-              :value="nrepeating.thursday"
-              @input="$emit('update:nrepeating.thursday', $event)"
-              :class="!nrepeating.thursday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.thursday = !nrepeating.thursday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="19"
-              :value="nrepeating.friday"
-              @input="$emit('update:nrepeating.friday', $event)"
-              :class="!nrepeating.friday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.friday = !nrepeating.friday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="20"
-              :value="nrepeating.saturday"
-              @input="$emit('update:nrepeating.saturday', $event)"
-              :class="!nrepeating.saturday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.saturday = !nrepeating.saturday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="21"
-              :value="nrepeating.sunday"
-              @input="$emit('update:nrepeating.sunday', $event)"
-              :class="!nrepeating.sunday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.sunday = !nrepeating.sunday"
-            />
-          </div>
-
-          <div>
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="22"
-              :value="nrepeating.monday"
-              @input="$emit('update:nrepeating.monday', $event)"
-              :class="!nrepeating.monday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.monday = !nrepeating.monday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="23"
-              :value="nrepeating.tuesday"
-              @input="$emit('update:nrepeating.tuesday', $event)"
-              :class="!nrepeating.tuesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.tuesday = !nrepeating.tuesday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="24"
-              :value="nrepeating.wednesday"
-              @input="$emit('update:nrepeating.wednesday', $event)"
-              :class="!nrepeating.wednesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.wednesday = !nrepeating.wednesday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="25"
-              :value="nrepeating.thursday"
-              @input="$emit('update:nrepeating.thursday', $event)"
-              :class="!nrepeating.thursday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.thursday = !nrepeating.thursday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="26"
-              :value="nrepeating.friday"
-              @input="$emit('update:nrepeating.friday', $event)"
-              :class="!nrepeating.friday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.friday = !nrepeating.friday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="27"
-              :value="nrepeating.saturday"
-              @input="$emit('update:nrepeating.saturday', $event)"
-              :class="!nrepeating.saturday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.saturday = !nrepeating.saturday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="28"
-              :value="nrepeating.sunday"
-              @input="$emit('update:nrepeating.sunday', $event)"
-              :class="!nrepeating.sunday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.sunday = !nrepeating.sunday"
-            />
-          </div>
-
-          <div>
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="29"
-              :value="nrepeating.monday"
-              @input="$emit('update:nrepeating.monday', $event)"
-              :class="!nrepeating.monday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.monday = !nrepeating.monday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="30"
-              :value="nrepeating.tuesday"
-              @input="$emit('update:nrepeating.tuesday', $event)"
-              :class="!nrepeating.tuesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.tuesday = !nrepeating.tuesday"
-            />
-            <q-btn
-              unelevated
-              flat
-              dense
-              text-color="primary"
-              label="31"
-              :value="nrepeating.wednesday"
-              @input="$emit('update:nrepeating.wednesday', $event)"
-              :class="!nrepeating.wednesday ? 'bg-red-1' : 'bg-green-1'"
-              @click="nrepeating.wednesday = !nrepeating.wednesday"
-            />
-          </div>
-        </div>
-
         <div v-if="byDayNumber">
           <q-btn
             unelevated
@@ -574,9 +216,9 @@ export default {
   data() {
     return {
       daily: false,
-      monthly: false,
       byDayNumber: false,
       everyNumDays: false,
+      everyMonth: false
     };
   },
   computed: {
@@ -589,13 +231,13 @@ export default {
         numberOfDays = value;
       }
     },
-    everyMonth: {
+    monthly: {
       get() {
-        return this.nrepeating.monthly;
+        return this.nrepeating.weekly;
       },
       set(value) {
-        this.nrepeating.monthly = value;
-        //everyMonth = value;
+        this.nrepeating.weekly = value;
+        everyMonth = value;
       }
     }
   }
